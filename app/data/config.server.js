e8b9d1f744d2cb7951bc1ae6b4fbcf38805f96ca
Barney Boisvert <bboisvert@gmail.com>
2014-06-05 18:03:19 +0000

    actually put the google analytics stuff in the right spot for production

--- app/data/config.server.js
+++ app/data/config.server.js
@@ -3,7 +3,9 @@ window.ThatOneFeed = {
     dataExtension: ".cfm",
     ga: {
         id: 'UA-379808-14',
-        env: 'prod',
-        config: 'auto'
+        env: 'dev',
+        config: {
+            cookieDomain: 'none'
+        }
     }
 };
--- server/config.js
+++ server/config.js
@@ -1,4 +1,9 @@
 window.ThatOneFeed = {
     dataDir: "data/",
-    dataExtension: ".cfm"
+    dataExtension: ".cfm",
+    ga: {
+        id: 'UA-379808-14',
+        env: 'prod',
+        config: 'auto'
+    }
 };
